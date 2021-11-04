a363bcb186c5f3ca40598bb6d78999fb2a631cde
Laila Chammaa <laylachammaa@gmail.com>
2021-11-03 21:25:35 -0400

    setting up model and dtos

---
+++ app/src/main/java/com/example/cryptocurrencyapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.cryptocurrencyapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.example.cryptocurrencyapp.iu.theme.CryptocurrencyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptocurrencyAppTheme {
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}
